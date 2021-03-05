245a39e894e67b8fe59e8938140ecf87fc5b3d81
Josh Suson <joshsuson@Joshs-Mac-mini.local>
2021-03-05 17:08:42 -0500

    First Commit

---
+++ schemas/job.js
@@ -0,0 +1,58 @@
+export default {
+  name: "job",
+  title: "Job",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 100,
+      },
+    },
+    {
+      name: "liveUrl",
+      title: "Live URL",
+      type: "url",
+    },
+    {
+      name: "projectTask",
+      title: "Project Task",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "projectSolution",
+      title: "Project Solution",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "technologiesUsed",
+      title: "Technologies Used",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "technology" }] }],
+    },
+    {
+      name: "skillsUsed",
+      title: "Skills Used",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "featuredImage",
+      title: "Featured Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
---
+++ schemas/technology.js
@@ -0,0 +1,17 @@
+export default {
+  name: "technology",
+  title: "Technology",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "color",
+      title: "Color",
+      type: "string",
+    },
+  ],
+};
